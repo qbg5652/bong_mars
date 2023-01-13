bd2e3f2b7c3358ab4d5db4d7a11038a9ce057243
qbg5652 <qbg5652@naver.com>
2023-01-13 12:30:17 +0900

    화성땅구매하기

---
+++ app.py
@@ -0,0 +1,34 @@
+import certifi
+ca = certifi.where()
+
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://bongdroid:qhdrbs88!@cluster0.hecgbmx.mongodb.net/Cluster0?retryWrites=true&w=majority", tlsCAFile = ca)
+
+db = client.pracdb
+
+
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/mars", methods=["POST"])
+def web_mars_post():
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+
+    doc = {'name': name_receive, 'address': address_receive, 'size': size_receive}
+    db.mars.insert_one(doc)
+
+    return jsonify({'msg': '주문 완료!'})
+
+@app.route("/mars", methods=["GET"])
+def web_mars_get():
+    order_list = list(db.mars.find({}, {'_id': False}))
+    return jsonify({'orders': order_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
